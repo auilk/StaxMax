3bc8f640bd6540b6210984594fbe1a82b532642c
auilk <abdelghafour.lbaz01@gmail.com>
2026-01-17 23:15:39 +0100

    Initialize Expo project with Tailwind

---
+++ app/Index.jsx
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="w-full h-full flex items-center justify-center">
+      <Text className="text-7xl font-bold">Hello</Text>
+    </View>
+  );
+}
