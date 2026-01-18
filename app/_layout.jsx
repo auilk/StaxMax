4e6669b7b3d5c8ce476c844ae66da6488493697d
auilk <abdelghafour.lbaz01@gmail.com>
2026-01-18 16:39:38 +0100

    Immersive mode setup

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,28 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { StatusBar } from "react-native";
+import { AppState } from "react-native";
+import * as NavigationBar from "expo-navigation-bar";
 import "../global.css";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function RootLayout()
+{
+  useEffect(() =>
+  {
+    const ImerssiveMode = async () => 
+    {
+      await NavigationBar.setVisibilityAsync("hidden");
+      StatusBar.setHidden(true);
+    };
+    ImerssiveMode();
+
+    const subsription = AppState.addEventListener("change", (state) =>
+    {
+      if (state == "active") ImerssiveMode();
+    });
+
+    return () => subsription.remove();
+  }, []);
+
+  return <Stack screenOptions={{ headerShown: false }}/>;
 }
